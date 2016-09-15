6601bbcbe6ffb1d3840ee09dbdf0e2a286751955
Clément Bourges <clement.bourges@yahoo.fr>
2016-09-15 11:20:49 +0200

    Fin du CRUD

---
+++ src/AppBundle/Form/UtilisateurType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UtilisateurType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('pseudo')
+            ->add('nom')
+            ->add('prenom')
+            ->add('mail')
+            ->add('dateNaissance', 'datetime')
+            ->add('ville')
+            ->add('twitter')
+            ->add('facebook')
+            ->add('avatar')
+            ->add('signature')
+            ->add('description')
+            ->add('abonnements')
+            ->add('episodesVisionnes')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Utilisateur'
+        ));
+    }
+}
