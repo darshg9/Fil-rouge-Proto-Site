64faec29774c2012f36c24fa6f64b02affb4d555
paul <paulooooo7@hotmail.com>
2016-09-19 16:58:20 +0200

    Moderation ended

--- src/AppBundle/Controller/ModerationController.php
+++ src/AppBundle/Controller/ModerationController.php
@@ -21,15 +21,15 @@ class ModerationController extends Controller
         $acteurs_not_valide = $em->getRepository('AppBundle:Acteur')->findActeursNotActive();
         $utilisateurs = $em->getRepository('AppBundle:Utilisateur')->findAll();
         $utilisateurs_ban = $em->getRepository('AppBundle:Utilisateur')->findUtilisateurBan();
-        $utilisateurs_role = $em->getRepository('AppBundle:Utilisateur')->findUtilisateurRole('ROLE');
+        $utilisateurs_role = $em->getRepository('AppBundle:Utilisateur')->findUtilisateurRole('ROLE_ADMIN');
         $critiques_signale = $em->getRepository('AppBundle:Critique')->findCritiqueSignale();
 
         return $this->render('moderation/index.html.twig', array(
                     'series' => $series_not_valide,
                     'acteurs' => $acteurs_not_valide,
-                    'utilisateur' => $utilisateurs,
-                    'utilisateur_ban' => $utilisateurs_ban,
-                    'utilisateur_role' => $utilisateurs_role,
+                    'utilisateurs' => $utilisateurs,
+                    'utilisateurs_ban' => $utilisateurs_ban,
+                    'utilisateurs_role' => $utilisateurs_role,
                     'critiques_signale' => $critiques_signale,
         ));
     }
